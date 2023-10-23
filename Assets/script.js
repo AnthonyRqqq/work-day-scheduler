4224cf991892ac391ce6467fbccfb503afc13a0f
unknown <anthony_rosati@yahoo.com>
2023-10-22 20:06:38 -0500

    Added code to save input to local storage as well as display it for each text area

--- Assets/script.js
+++ Assets/script.js
@@ -20,8 +20,18 @@ $(function () {
     $("#hour-17"),
   ]
 
-  // Sets all timeblocks to an integer in military time for comparison with dayJS
+  
   for (var i = 0; i < hours.length; i++) {
+
+    // Displays all saved content if applicable to hour blocks
+    var getSavedInput = localStorage.getItem($(hours[i]).attr("id"));
+    if (getSavedInput === null) {
+      $(hours[i]).children("textarea").text("");
+    } else {
+      $(hours[i]).children("textarea").text(getSavedInput);
+    }
+
+     // Sets all timeblocks to an integer in military time for comparison with dayJS
     var currentHour = parseInt($(hours[i]).text())
     if (currentHour < 9) {
       currentHour += 12;
@@ -36,20 +46,10 @@ $(function () {
       $(hours[i]).children("textarea").addClass("future");
     }
   }
-  
-  var hourRow = $(".time-block");
-  hourRow.on("click", ".saveBtn", function(event){
 
+  // Uses event delegation to listen for the save button being clicked and saving content
+  var timeBlockEl = $(".time-block");
+  timeBlockEl.on("click", ".saveBtn", function(event) {
+    localStorage.setItem($(event.target).parent().attr("id"), $(event.target).siblings("textarea").val())
   })
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 });
